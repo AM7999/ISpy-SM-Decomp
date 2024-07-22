cc115fc3007a906427a60c200641d9e6e2fb3e8b
Angel Martin <AMartin12122019@Outlook.com>
2024-07-21 20:31:21 -0700

    whoops no project files!

--- ISpy Decomp/src/main.cpp
+++ ISpy Decomp/src/main.cpp
@@ -2,10 +2,13 @@
 
 
 int main(int argc, char argv) {
-	int var1;
-	
-	
+	int iVar1;
+	//undefined uVar2;
+	char cVar3;
+	int unaff_r13;
+	//undefined uVar4;
 
 	//OSInit();
-
+	
+	unaff_r13 + -0x2828 = argc;
 }
